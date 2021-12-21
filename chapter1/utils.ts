f5e5112ef962fea703efa1edf8eaec2e3b10f38e
wazimuhb <hesbonosoro1@gmail.com>
2021-12-21 13:53:07 +0300

    better developer experience

--- chapter1/utils.ts
+++ chapter1/utils.ts
@@ -1,5 +1,13 @@
-function calculateTotalPrice(product, quantity, discount) {
-	var priceWithoutDiscount = product.price * quantity;
+interface IProduct {
+	name: String;
+	unitPrice: number;
+}
+function calculateTotalPriceTS(
+	product: IProduct,
+	quantity: number,
+	discount: number
+): number {
+	var priceWithoutDiscount = product.unitPrice * quantity;
 	var discountAmount = priceWithoutDiscount * discount;
 	return priceWithoutDiscount - discountAmount;
 }

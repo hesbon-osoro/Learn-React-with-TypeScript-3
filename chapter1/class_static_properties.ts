01ec99aa462755747025090a998c897bff43ce50
wazimuhb <hesbonosoro1@gmail.com>
2021-12-24 00:36:56 +0300

    static method

---
+++ chapter1/class_static_properties.ts
@@ -0,0 +1,10 @@
+class OrderDetail14 {
+	product: Product;
+	quantity: number;
+
+	// static getTotal(discount: number): number{
+	//     const priceWithoutDiscount = this.product.price * this.quantity;
+	//     const discountAmount = priceWithoutDiscount * discount;
+	//     return priceWithoutDiscount - discountAmount;
+	// }
+}

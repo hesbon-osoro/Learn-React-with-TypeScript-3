9903f454c210bab16249b7dfe75e3f9f8a3791b3
wazimuhb <hesbonosoro1@gmail.com>
2021-12-23 22:30:25 +0300

    classes and their instances

--- chapter1/classes.ts
+++ chapter1/classes.ts
@@ -1,10 +1,28 @@
-class Product{
-    name: string;
-    price: number;
+class Product6 {
+	name: string;
+	price: number;
 }
 
-const table6 = new Product();
+class OrderDetail6 {
+	product: Product;
+	quantity: number;
 
-table6.name='Table'
-table6.price=100;
+	getTotal6(discount: number): number {
+		const priceWithoutDiscount = this.product.price * this.quantity;
+		const discountAmount = priceWithoutDiscount * discount;
+		return priceWithoutDiscount - discountAmount;
+	}
+}
 
+const table6 = new Product6();
+
+table6.name = 'Table';
+table6.price = 100;
+
+const orderDetail6 = new OrderDetail6();
+orderDetail6.product = table6;
+orderDetail6.quantity = 2;
+
+const total6 = orderDetail6.getTotal6(0.1);
+
+console.log(total6);

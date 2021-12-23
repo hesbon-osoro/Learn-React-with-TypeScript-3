769a270ad16b6b5424e8f209bad7f592680cde3e
wazimuhb <hesbonosoro1@gmail.com>
2021-12-24 00:02:50 +0300

    instance of class | cannot have access to private properties

--- chapter1/class_access_modifiers.ts
+++ chapter1/class_access_modifiers.ts
@@ -14,3 +14,8 @@ class OrderDetail12 {
 	}
 }
 
+const orderDetail12 = new OrderDetail12();
+
+// this is not possible
+// an instance of class cannot access private class properties
+// orderDetail12.deleted = true;

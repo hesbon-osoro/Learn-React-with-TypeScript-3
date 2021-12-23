06245dcc33437f13668404aa4b39869aca75e250
wazimuhb <hesbonosoro1@gmail.com>
2021-12-23 22:18:32 +0300

    replacing all interfaces with type aliases

--- chapter1/type_aliases.ts
+++ chapter1/type_aliases.ts
@@ -1,7 +1,27 @@
 type GetTotal = (discount: number) => number;
 
-interface OrderDetail5 {
-	product: Product;
+type Product5 = {
+	name: string;
+	price: number;
+};
+
+type OrderDetail5 = {
+	product: Product5;
 	quantity: number;
-	getTotal: GetTotal;
-}
+	getTotal: (discount: number) => number;
+};
+
+const table5: Product5 = {
+	name: 'Table',
+	price: 100,
+};
+
+const orderDetail5: OrderDetail5 = {
+	product: table5,
+	quantity: 2,
+	getTotal(discount: number): number {
+		const priceWithoutDiscount = this.product.price * this.quantity;
+		const discountAmount = priceWithoutDiscount * discount;
+		return priceWithoutDiscount - discountAmount;
+	},
+};

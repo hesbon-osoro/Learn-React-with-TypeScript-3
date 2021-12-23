e4ff7f6a1256e80c764d84eae3a0f2a9cfb17097
wazimuhb <hesbonosoro1@gmail.com>
2021-12-24 00:40:15 +0300

    making a static method work by passing dependencies as its paramters

--- chapter1/class_static_properties.ts
+++ chapter1/class_static_properties.ts
@@ -2,9 +2,17 @@ class OrderDetail14 {
 	product: Product;
 	quantity: number;
 
+	// this method has no access for the class properties
 	// static getTotal(discount: number): number{
 	//     const priceWithoutDiscount = this.product.price * this.quantity;
 	//     const discountAmount = priceWithoutDiscount * discount;
 	//     return priceWithoutDiscount - discountAmount;
 	// }
+
+	// to make it work we pass the dependencies as parameters
+	static getTotal(price: number, quantity: number, discount: number): number {
+		const priceWithoutDiscount = price * quantity;
+		const discountAmount = priceWithoutDiscount * discount;
+		return priceWithoutDiscount - discountAmount;
+	}
 }

a2e02573bf90b4daf266db3ffbec31f763ba1b4c
wazimuhb <hesbonosoro1@gmail.com>
2021-12-24 03:49:04 +0300

    transpiled js code with tuple accessed the array way

--- chapter2/dist/tuples.js
+++ chapter2/dist/tuples.js
@@ -6,4 +6,15 @@ let last3scores;
 last3scores = ['Tom', 10, 20, 30];
 let point;
 point = [100, 200, 100];
+// accessing tuple elements as array elements
+console.log(product[0]);
+console.log(product[1]);
+// using for loop
+for (const l in last3scores) {
+    if (l) {
+        console.log(last3scores[l]);
+    }
+}
+// using forEach array function
+last3scores.forEach(el => console.log(el));
 //# sourceMappingURL=tuples.js.map

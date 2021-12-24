885157f445b6225f0aaaf6d461f0f0c2552d1d8c
wazimuhb <hesbonosoro1@gmail.com>
2021-12-25 00:14:37 +0300

    App tsx file

---
+++ chapter3/react-ts-app/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+	return (
+		<div className="App">
+			<p>React and TypeScript</p>
+			<p>
+				<small>(create-react-app)</small>
+			</p>
+		</div>
+	);
+}
+
+export default App;

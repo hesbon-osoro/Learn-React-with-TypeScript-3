9d95710b2d01b8afa3c066e367aa8a23a6d647d8
wazimuhb <hesbonosoro1@gmail.com>
2021-12-24 15:27:52 +0300

    transpiled code

---
+++ chapter2/dist/empty_tuples.js
@@ -0,0 +1,9 @@
+const empty = [];
+const benScores = [];
+const samScores = [55];
+const janeScores = [55, 66];
+const jimScores = [55, 66, 77];
+export {};
+// the score is not allowed
+// const hbScores:Scores=[55,66,77,77]
+//# sourceMappingURL=empty_tuples.js.map

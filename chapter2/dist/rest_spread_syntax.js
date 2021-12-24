1d0d108b8e6d1701a8dd53017d4033f5b37ff9b5
wazimuhb <hesbonosoro1@gmail.com>
2021-12-24 15:15:47 +0300

    prettier format

--- chapter2/dist/rest_spread_syntax.js
+++ chapter2/dist/rest_spread_syntax.js
@@ -1,8 +1,8 @@
 function logScores(...scores) {
-    console.log(scores);
+	console.log(scores);
 }
 function logScores1(score1, score2, score3) {
-    console.log(score1, score2, score3);
+	console.log(score1, score2, score3);
 }
 const scores = [10, 20, 30];
 // logScores1(...scores: number[]);

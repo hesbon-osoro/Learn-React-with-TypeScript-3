538cb94057d64340e1c490921e51cc5370932481
wazimuhb <hesbonosoro1@gmail.com>
2021-12-24 14:18:24 +0300

    transpiled code

---
+++ chapter2/dist/rest_spread_syntax.js
@@ -0,0 +1,9 @@
+function logScores(...scores) {
+    console.log(scores);
+}
+function logScores1(score1, score2, score3) {
+    console.log(score1, score2, score3);
+}
+const scores = [10, 20, 30];
+// logScores1(...scores: number[]);
+//# sourceMappingURL=rest_spread_syntax.js.map

7742cd59de6e54f5a95dd168428bc8d086365d76
wazimuhb <hesbonosoro1@gmail.com>
2021-12-24 14:00:17 +0300

    another function with paramters

--- chapter2/src/rest_spread_syntax.ts
+++ chapter2/src/rest_spread_syntax.ts
@@ -1,3 +1,10 @@
 function logScores(...scores: number[]) {
 	console.log(scores);
 }
+
+function logScores1(score1: number, score2: number, score3: number) {
+	console.log(score1, score2, score3);
+}
+const scores: number[] = [10, 20, 30];
+
+// logScores1(...scores: number[]);

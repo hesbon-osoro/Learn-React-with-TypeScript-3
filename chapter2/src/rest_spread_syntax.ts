97db997b9fecb973d052411045d95cc668554335
wazimuhb <hesbonosoro1@gmail.com>
2021-12-24 15:13:46 +0300

    more of ts code

--- chapter2/src/rest_spread_syntax.ts
+++ chapter2/src/rest_spread_syntax.ts
@@ -1,6 +1,16 @@
-function logScores(...scores: number[]) {
+export type Scores = [string, ...number[]];
+
+function logNameAndScores(...scores: Scores) {
 	console.log(scores);
 }
+
+logNameAndScores('Billy', 60, 70, 75);
+
+function logScores(...scores: [...number[]]) {
+	console.log(scores);
+}
+
+logScores(50, 85, 75);
 
 function logScores1(score1: number, score2: number, score3: number) {
 	console.log(score1, score2, score3);

a15dee1d9043fc201b59f7ae83213aeff4d04d6e
wazimuhb <hesbonosoro1@gmail.com>
2021-12-25 16:34:53 +0300

    Confirm component update with state

--- chapter3/using-components/src/Confirm.tsx
+++ chapter3/using-components/src/Confirm.tsx
@@ -1,19 +1,45 @@
 import React, { Component } from 'react';
 import './Confirm.css';
-class Confirm extends Component {
+interface IProps {
+	open: boolean;
+	title: string;
+	content: string;
+	cancelCaption?: string;
+	okCaption?: string;
+	onOkClick: () => void;
+	onCancelClick: () => void;
+}
+class Confirm extends Component<IProps> {
+	public static defaultProps = { cancelCaption: 'Cancel', okCaption: 'Okay' };
+	private handleOkClick = () => {
+		this.props.onOkClick();
+	};
+	private handleCancelClick = () => {
+		this.props.onCancelClick();
+	};
 	public render() {
 		return (
-			<div className="confirm-wrapper confirm-visible">
+			<div
+				className={
+					this.props.open
+						? 'confirm-wrapper confirm-visible'
+						: 'confirm-wrapper'
+				}
+			>
 				<div className="confirm-container">
 					<div className="confirm-title-container">
-						<span>Title</span>
+						<span>{this.props.title}</span>
 					</div>
 					<div className="confirm-content-container">
-						<p>Content</p>
+						<p>{this.props.content}</p>
 					</div>
 					<div className="confirm-buttons-container">
-						<button className="confirm-cancel">Cancel</button>
-						<button className="confirm-ok">Okay</button>
+						<button className="confirm-cancel" onClick={this.handleCancelClick}>
+							{this.props.cancelCaption}
+						</button>
+						<button className="confirm-ok" onClick={this.handleOkClick}>
+							{this.props.okCaption}
+						</button>
 					</div>
 				</div>
 			</div>

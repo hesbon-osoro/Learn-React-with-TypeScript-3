d4365c156426f8044e6e076bf38ed4e0d5ce9b87
wazimuhb <hesbonosoro1@gmail.com>
2022-01-02 21:40:10 +0300

    implement search | props | state

--- chapter4/react-router-shop/src/ProductsPage.tsx
+++ chapter4/react-router-shop/src/ProductsPage.tsx
@@ -1,21 +1,32 @@
 import React, { Component } from 'react';
 import { IProduct, products } from './ProductsData';
-import { Link } from 'react-router-dom';
-
+import { Link, RouteComponentProps } from 'react-router-dom';
+import 'url-search-params-polyfill';
 interface IState {
 	products: IProduct[];
+	search: string;
 }
 
-class ProductsPage extends Component<{}, IState> {
-	public constructor(props: {}) {
+class ProductsPage extends Component<RouteComponentProps, IState> {
+	public constructor(props: RouteComponentProps) {
 		super(props);
-		this.state = { products: [] };
+		this.state = {
+			products: [],
+			search: '',
+		};
 	}
 
 	public componentDidMount() {
 		this.setState({ products });
 	}
-
+	public static getDerivedStateFromProps(
+		props: RouteComponentProps,
+		state: IState
+	) {
+		const searchParams = new URLSearchParams(props.location.search);
+		const search = searchParams.get('search') || '';
+		return { products: state.products, search };
+	}
 	public render() {
 		return (
 			<div className="page-container">
@@ -23,11 +34,23 @@ class ProductsPage extends Component<{}, IState> {
 					Welcome to React Shop where you can get all your tools for ReactJS!
 				</p>
 				<ul className="product-list">
-					{this.state.products.map(product => (
-						<li key={product.id} className="product-list-item">
-							<Link to={`/products/${product.id}`}> {product.name}</Link>
-						</li>
-					))}
+					{this.state.products.map(product => {
+						if (
+							!this.state.search ||
+							(this.state.search &&
+								product.name
+									.toLowerCase()
+									.indexOf(this.state.search.toLowerCase()) > -1)
+						) {
+							return (
+								<li key={product.id} className="product-list-item">
+									<Link to={`/products/${product.id}`}> {product.name}</Link>
+								</li>
+							);
+						} else {
+							return null;
+						}
+					})}
 				</ul>
 			</div>
 		);

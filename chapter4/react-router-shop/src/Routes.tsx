1f3d9a032c9352bc26ade64adc3721ce0b98e19a
wazimuhb <hesbonosoro1@gmail.com>
2022-01-03 22:44:27 +0300

    implementing lazy loading of routes

--- chapter4/react-router-shop/src/Routes.tsx
+++ chapter4/react-router-shop/src/Routes.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from 'react';
+import React, { FC, Suspense, lazy, useState } from 'react';
 import {
 	BrowserRouter as Router,
 	Redirect,
@@ -8,12 +8,13 @@ import {
 } from 'react-router-dom';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
-import AdminPage from './AdminPage';
 import Header from './Header';
 import ProductsPage from './ProductsPage';
 import ProductPage from './ProductPage';
 import NotFoundPage from './NotFoundPage';
 import LoginPage from './LoginPage';
+
+const AdminPage = lazy(() => import('./AdminPage'));
 
 const RoutesWrap: FC = () => {
 	return (
@@ -38,7 +39,15 @@ const Routes: FC<RouteComponentProps> = props => {
 						<Route exact={true} path="/products" component={ProductsPage} />
 						<Route path="/products/:id" component={ProductPage} />
 						<Route path="/admin">
-							{loggedIn ? <AdminPage /> : <Redirect to="/login" />}
+							{loggedIn ? (
+								<Suspense
+									fallback={<div className="page-container">Loading ...</div>}
+								>
+									<AdminPage />
+								</Suspense>
+							) : (
+								<Redirect to="/login" />
+							)}
 						</Route>
 						<Route path="/login" component={LoginPage} />
 						<Route component={NotFoundPage} />

d381179bab10656b1d2bdc0d4fd0535207bac9a4
wazimuhb <hesbonosoro1@gmail.com>
2022-01-04 00:00:38 +0300

    using the several strings

--- chapter5/string_literal_union.ts
+++ chapter5/string_literal_union.ts
@@ -3,3 +3,5 @@ export type Control = 'Textbox' | 'DropDown' | 'DatePicker' | 'NumberSlider';
 export let notes: Control;
 notes = 'Textbox';
 notes = 'DropDown';
+notes = 'DatePicker';
+notes = 'NumberSlider';

40e1ff33b7c3bdeb4bdd9c42c20319d831dfe4f3
wazimuhb <hesbonosoro1@gmail.com>
2022-01-15 12:12:10 +0300

    consuming and passing data to the presentation component

--- chapter6/react-router-shop/src/ProductPage.tsx
+++ chapter6/react-router-shop/src/ProductPage.tsx
@@ -1,6 +1,8 @@
 import React, { Component, Fragment } from 'react';
 import { Prompt, RouteComponentProps } from 'react-router-dom';
 import { IProduct, products } from './ProductsData';
+import Product from './Product';
+
 type Props = RouteComponentProps<{ id: string }>;
 
 interface IState {
@@ -33,19 +35,11 @@ class ProductPage extends Component<Props, IState> {
 			<div className="page-container">
 				<Prompt when={!this.state.added} message={this.navAwayMessage} />
 				{product ? (
-					<Fragment>
-						<h1>{product.name}</h1>
-						<p>{product.description}</p>
-						<p className="product-price">
-							{new Intl.NumberFormat('en-US', {
-								currency: 'USD',
-								style: 'currency',
-							}).format(product.price)}
-						</p>
-						{!this.state.added && (
-							<button onClick={this.handleAddClick}>Add to basket</button>
-						)}
-					</Fragment>
+					<Product
+						product={product}
+						inBasket={this.state.added}
+						onAddToBasket={this.handleAddClick}
+					/>
 				) : (
 					<p>Product not found</p>
 				)}

a3766964f08e1fd3f5ae0176ef8b0d3a8612aa9d
wazimuhb <hesbonosoro1@gmail.com>
2022-01-17 12:16:27 +0300

    refactoring the Tabs component to allow leveraging the compound component pattern

--- chapter6/react-router-shop/src/Tabs.tsx
+++ chapter6/react-router-shop/src/Tabs.tsx
@@ -1,41 +1,68 @@
+// Basic component
+// import React from 'react';
+
+// export interface IProps {
+// 	headings: string[];
+// }
+// export interface IState {
+// 	activeHeading: string;
+// }
+
+// class Tabs extends React.Component<IProps, IState> {
+// 	public constructor(props: IProps) {
+// 		super(props);
+// 		this.state = {
+// 			activeHeading:
+// 				this.props.headings && this.props.headings.length > 0
+// 					? this.props.headings[0]
+// 					: '',
+// 		};
+// 	}
+
+// 	private handleTabClick = (e: React.MouseEvent<HTMLLIElement>) => {
+// 		const li = e.target as HTMLLIElement;
+// 		const heading: string = li.textContent ? li.textContent : '';
+// 		this.setState({ activeHeading: heading });
+// 	};
+// 	public render() {
+// 		return (
+// 			<ul className="tabs">
+// 				{this.props.headings.map(heading => (
+// 					<li
+// 						onClick={this.handleTabClick}
+// 						className={heading === this.state.activeHeading ? 'active' : ''}
+// 					>
+// 						{heading}
+// 					</li>
+// 				))}
+// 			</ul>
+// 		);
+// 	}
+// }
+
+// export default Tabs;
+
+// Refactored component
 import React from 'react';
-
-export interface IProps {
-	headings: string[];
-}
 export interface IState {
-	activeHeading: string;
+	activeName: string;
 }
 
-class Tabs extends React.Component<IProps, IState> {
-	public constructor(props: IProps) {
-		super(props);
-		this.state = {
-			activeHeading:
-				this.props.headings && this.props.headings.length > 0
-					? this.props.headings[0]
-					: '',
-		};
-	}
-
+export interface ITabProps {
+	name: string;
+	initialActive?: boolean;
+}
+class Tabs extends React.Component<{}, IState> {
+	public static Tab: React.FC<ITabProps> = props => (
+		<li>TODO - render the child nodes</li>
+	);
 	private handleTabClick = (e: React.MouseEvent<HTMLLIElement>) => {
 		const li = e.target as HTMLLIElement;
 		const heading: string = li.textContent ? li.textContent : '';
-		this.setState({ activeHeading: heading });
+		this.setState({ activeName: heading });
 	};
 	public render() {
-		return (
-			<ul className="tabs">
-				{this.props.headings.map(heading => (
-					<li
-						onClick={this.handleTabClick}
-						className={heading === this.state.activeHeading ? 'active' : ''}
-					>
-						{heading}
-					</li>
-				))}
-			</ul>
-		);
+		return <p>Hello</p>;
 	}
 }
 

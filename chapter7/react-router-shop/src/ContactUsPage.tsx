facd67091407cd43f7cb3e50dced78e55eb3f921
wazimuhb <hesbonosoro1@gmail.com>
2022-01-24 00:23:00 +0300

    contact us page container

---
+++ chapter7/react-router-shop/src/ContactUsPage.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export class ContactUsPage extends React.Component {
+	public render() {
+		return (
+			<div className="page-container">
+				<h1>Contact Us</h1>
+				<p>
+					If you enter your details we'll get back to you as soon as we can.
+				</p>
+			</div>
+		);
+	}
+}
+
+export default ContactUsPage;

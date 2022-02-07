59769db62685f64385382feb04384fa5ed1b133c
wazimuhb <hesbonosoro1@gmail.com>
2022-02-07 21:36:41 +0300

    handling input change

--- chapter7/react-router-shop/src/ContactUs.tsx
+++ chapter7/react-router-shop/src/ContactUs.tsx
@@ -2,17 +2,29 @@ import React from 'react';
 
 export interface IProps {
 	name: string;
+	onNameChange: (name: string) => void;
 	email: string;
+	onEmailChange: (email: string) => void;
 	reason: string;
+	onReasonChange: (reason: string) => void;
 	notes: string;
+	onNotesChange: (notes: string) => void;
 }
 
 const ContactUs: React.FC<IProps> = props => {
+	const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		props.onNameChange(e.currentTarget.value);
+	};
 	return (
 		<form className="form" noValidate={true}>
 			<div className="form-group">
 				<label htmlFor="name">Your name</label>
-				<input type="text" id="name" value={props.name} />
+				<input
+					type="text"
+					id="name"
+					value={props.name}
+					onChange={handleNameChange}
+				/>
 			</div>
 		</form>
 	);

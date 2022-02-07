15417c075557deaf557244655f9ae3f8a51bc151
wazimuhb <hesbonosoro1@gmail.com>
2022-02-08 00:48:40 +0300

    using Context in the app

--- chapter7/react-router-shop/src/Form.tsx
+++ chapter7/react-router-shop/src/Form.tsx
@@ -18,7 +18,12 @@ export interface IFieldProps {
 	type?: 'Text' | 'Email' | 'Select' | 'TextArea';
 	options?: string[];
 }
-
+export interface IFormContext {
+	values: IValues;
+}
+const FormContext = React.createContext<IFormContext>({
+	values: {},
+});
 export class Form extends React.Component<IFormProps, IState> {
 	constructor(props: IFormProps) {
 		super(props);
@@ -29,30 +34,45 @@ export class Form extends React.Component<IFormProps, IState> {
 	public static Field: React.FC<IFieldProps> = props => {
 		const { name, label, type, options } = props;
 		return (
-			<div className="form-group">
-				<label htmlFor={name}>{label}</label>
-				{(type === 'Text' || type === 'Email') && (
-					<input type={type?.toLowerCase()} id={name} />
+			<FormContext.Consumer>
+				{context => (
+					<div className="form-group">
+						<label htmlFor={name}>{label}</label>
+						{(type === 'Text' || type === 'Email') && (
+							<input
+								type={type?.toLowerCase()}
+								id={name}
+								value={context.values[name]}
+							/>
+						)}
+						{type === 'TextArea' && (
+							<textarea id={name} value={context.values[name]} />
+						)}
+						{type === 'Select' && (
+							<select value={context.values[name]}>
+								{options &&
+									options.map(option => (
+										<option key={option} value={option}>
+											{option}
+										</option>
+									))}
+							</select>
+						)}
+					</div>
 				)}
-				{type === 'TextArea' && <textarea id={name} />}
-				{type === 'Select' && (
-					<select>
-						{options &&
-							options.map(option => (
-								<option key={option} value={option}>
-									{option}
-								</option>
-							))}
-					</select>
-				)}
-			</div>
+			</FormContext.Consumer>
 		);
 	};
 	public render() {
+		const context: IFormContext = {
+			values: this.state.values,
+		};
 		return (
-			<form className="form" noValidate={true}>
-				{this.props.children}
-			</form>
+			<FormContext.Provider value={context}>
+				<form className="form" noValidate={true}>
+					{this.props.children}
+				</form>
+			</FormContext.Provider>
 		);
 	}
 }

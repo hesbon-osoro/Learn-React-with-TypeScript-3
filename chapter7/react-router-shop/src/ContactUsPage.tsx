e1999a4ad1eb8ded2018637c23f95841759c9b51
wazimuhb <hesbonosoro1@gmail.com>
2022-03-01 00:16:28 +0300

    handleSubmit

--- chapter7/react-router-shop/src/ContactUsPage.tsx
+++ chapter7/react-router-shop/src/ContactUsPage.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
 
 import ContactUs from './ContactUs';
+import { ISubmitResult, IValues } from './Form';
 
 export class ContactUsPage extends React.Component<{}, {}> {
+	private handleSubmit = async (values: IValues): Promise<ISubmitResult> => {
+		const wait = (ms: number): Promise<void> => {
+			return new Promise(res => setTimeout(res, ms));
+		};
+		await wait(1000);
+		//simulate asynchronous web API call
+		return {
+			success: true,
+		};
+	};
+
 	public render() {
 		return (
 			<div className="page-container">
@@ -10,7 +22,7 @@ export class ContactUsPage extends React.Component<{}, {}> {
 				<p>
 					If you enter your details we'll get back to you as soon as we can.
 				</p>
-				<ContactUs />
+				<ContactUs onSubmit={this.handleSubmit} />
 			</div>
 		);
 	}

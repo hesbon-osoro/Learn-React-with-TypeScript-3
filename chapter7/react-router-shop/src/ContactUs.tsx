91cbd8229520b9691d646b1b465975c66883e5aa
wazimuhb <hesbonosoro1@gmail.com>
2022-02-28 11:31:30 +0300

    refactored ContactUs component

--- chapter7/react-router-shop/src/ContactUs.tsx
+++ chapter7/react-router-shop/src/ContactUs.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
-
+import { Form } from './Form';
 const ContactUs: React.FC = () => {
-	
-	return null;
+	return (
+		<Form defaultValues={{ name: '', email: '', reason: 'Support', notes: '' }}>
+			<Form.Field name="name" label="Your name" />
+			<Form.Field name="email" label="Your email address" type="Email" />
+			<Form.Field
+				name="reason"
+				label="Reason you need to contact us"
+				type="Select"
+				options={['Marketing', 'Support', 'Feedback', 'Jobs', 'Other']}
+			/>
+			<Form.Field name="notes" label="Additional notes" type="TextArea" />
+		</Form>
+	);
 };
 
 export default ContactUs;

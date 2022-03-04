07a4771345ee84bff409017913618d59f48cf30f
wazimuhb <hesbonosoro1@gmail.com>
2022-03-04 14:50:46 +0300

    connecting components to the store

--- chapter8/react-router-shop/src/index.tsx
+++ chapter8/react-router-shop/src/index.tsx
@@ -2,5 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Routes from './Routes';
+import { Provider } from 'react-redux';
+import { Store } from 'redux';
+import configureStore from './Store';
+import { IApplicationState } from './Store';
 
-ReactDOM.render(<Routes />, document.getElementById('root') as HTMLElement);
+interface IProps {
+	store: Store<IApplicationState>;
+}
+const Root: React.FC<IProps> = props => {
+	return (
+		<Provider store={props.store}>
+			<Routes />
+		</Provider>
+	);
+};
+const store = configureStore();
+
+ReactDOM.render(
+	<Root store={store} />,
+	document.getElementById('root') as HTMLElement
+);

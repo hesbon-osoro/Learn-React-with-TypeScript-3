22d0cb4f51cf23876c361cb91adb3274be4bed15
wazimuhb <hesbonosoro1@gmail.com>
2022-03-05 00:00:54 +0300

    state for a single product

--- chapter8/react-router-shop/src/ProductsTypes.ts
+++ chapter8/react-router-shop/src/ProductsTypes.ts
@@ -2,6 +2,7 @@ import { IProduct } from './ProductsData';
 
 export enum ProductsActionTypes {
 	GETALL = 'PRODUCTS/GETALL',
+	GETSINGLE = 'PRODUCTS/GETSINGLE',
 	LOADING = 'PRODUCTS/LOADING',
 }
 
@@ -9,13 +10,21 @@ export interface IProductsGetAllAction {
 	type: ProductsActionTypes.GETALL;
 	products: IProduct[];
 }
+export interface IProductsGetSingleAction {
+	type: ProductsActionTypes.GETSINGLE;
+	product: IProduct;
+}
 export interface IProductsLoadingAction {
 	type: ProductsActionTypes.LOADING;
 }
 
-export type ProductsActions = IProductsGetAllAction | IProductsLoadingAction;
+export type ProductsActions =
+	| IProductsGetAllAction
+	| IProductsGetSingleAction
+	| IProductsLoadingAction;
 
 export interface IProductsState {
 	readonly products: IProduct[];
+	readonly currentProduct: IProduct | null;
 	readonly productsLoading: boolean;
 }

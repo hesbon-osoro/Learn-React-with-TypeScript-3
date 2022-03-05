871f691be4ec1bdb36491a8950ad17d43f4967f7
wazimuhb <hesbonosoro1@gmail.com>
2022-03-05 15:09:25 +0300

    refactoring to class based and connecting with state and basket summary component

--- chapter8/react-router-shop/src/Header.tsx
+++ chapter8/react-router-shop/src/Header.tsx
@@ -8,61 +8,79 @@ import React, {
 import { NavLink, RouteComponentProps, withRouter } from 'react-router-dom';
 import 'url-search-params-polyfill';
 import logo from './logo.svg';
+import BasketSummary from './BasketSummary';
+import { connect } from 'react-redux';
+import { IApplicationState } from './Store';
 
-const Header: FC<RouteComponentProps> = props => {
-	const [search, setSearch] = useState('');
-	useEffect(() => {
-		const searchParams = new URLSearchParams(props.location.search);
-		setSearch(searchParams.get('search') || '');
-	}, []);
+interface IState {
+	search: string;
+}
+interface IProps extends RouteComponentProps {
+	basketCount: number;
+}
+class Header extends React.Component<IProps, IState> {
+	public constructor(props: IProps) {
+		super(props);
+		this.state = { search: '' };
+	}
+	public componentDidMount() {
+		const searchParams = new URLSearchParams(this.props.location.search);
+		const search = searchParams.get('search') || '';
+		this.setState({ search });
+	}
 
-	const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
-		setSearch(e.currentTarget.value);
+	private handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
+		this.setState({ search: e.currentTarget.value });
 	};
 
-	const handleSearchKeydown = (e: KeyboardEvent<HTMLInputElement>) => {
+	private handleSearchKeydown = (e: KeyboardEvent<HTMLInputElement>) => {
 		if (e.key === 'Enter') {
-			props.history.push(`/products?search=${search}`);
+			this.props.history.push(`/products?search=${this.state.search}`);
 		}
 	};
-	return (
-		<header className="header">
-			<div className="search-container">
-				<input
-					type="search"
-					placeholder="search"
-					value={search}
-					onChange={handleSearchChange}
-					onKeyDown={handleSearchKeydown}
-				/>
-			</div>
-			<img src={logo} className="header-logo" alt="logo" />
-			<h1 className="header-title">React Shop</h1>
-			<nav>
-				<NavLink
-					to="/products"
-					className="header-link"
-					activeClassName="header-link-active"
-				>
-					Products
-				</NavLink>
-				<NavLink
-					to="/contactus"
-					className="header-link"
-					activeClassName="header-link-active"
-				>
-					Contact Us
-				</NavLink>
-				<NavLink
-					to="/admin"
-					className="header-link"
-					activeClassName="header-link-active"
-				>
-					Admin
-				</NavLink>
-			</nav>
-		</header>
-	);
+	public render() {
+		return (
+			<header className="header">
+				<div className="search-container">
+					<input
+						type="search"
+						placeholder="search"
+						value={this.state.search}
+						onChange={this.handleSearchChange}
+						onKeyDown={this.handleSearchKeydown}
+					/>
+					<BasketSummary count={this.props.basketCount} />
+				</div>
+				<img src={logo} className="header-logo" alt="logo" />
+				<h1 className="header-title">React Shop</h1>
+				<nav>
+					<NavLink
+						to="/products"
+						className="header-link"
+						activeClassName="header-link-active"
+					>
+						Products
+					</NavLink>
+					<NavLink
+						to="/contactus"
+						className="header-link"
+						activeClassName="header-link-active"
+					>
+						Contact Us
+					</NavLink>
+					<NavLink
+						to="/admin"
+						className="header-link"
+						activeClassName="header-link-active"
+					>
+						Admin
+					</NavLink>
+				</nav>
+			</header>
+		);
+	}
+}
+const mapStateToProps = (store: IApplicationState) => {
+	return { basketCount: store.basket.products.length };
 };
-
-export default withRouter(Header);
+export default connect(mapStateToProps)(withRouter(Header));

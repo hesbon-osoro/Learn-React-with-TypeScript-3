ee20758b658d29d9067d33951ce6da91436199c5
wazimuhb <hesbonosoro1@gmail.com>
2022-03-05 14:27:31 +0300

    basket summary component

---
+++ chapter8/react-router-shop/src/BasketSummary.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+interface IProps {
+	count: number;
+}
+
+const BasketSummary: React.FC<IProps> = props => {
+	return <div className="basket-summary">{props.count}</div>;
+};
+
+export default BasketSummary;

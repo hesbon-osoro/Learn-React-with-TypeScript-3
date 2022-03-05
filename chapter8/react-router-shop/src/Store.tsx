8fd4635bc0d3e0e85b576523b3ac57f82719d656
wazimuhb <hesbonosoro1@gmail.com>
2022-03-05 13:42:33 +0300

    adding basket to the store

--- chapter8/react-router-shop/src/Store.tsx
+++ chapter8/react-router-shop/src/Store.tsx
@@ -2,12 +2,16 @@ import { applyMiddleware, combineReducers, createStore, Store } from 'redux';
 import thunk from 'redux-thunk';
 import { productsReducer } from './ProductsReducers';
 import { IProductsState } from './ProductsTypes';
+import { basketReducer } from './BasketReducer';
+import { IBasketState } from './BasketTypes';
 
 export interface IApplicationState {
+	basket: IBasketState;
 	products: IProductsState;
 }
 
 const rootReducer = combineReducers<IApplicationState>({
+	basket: basketReducer,
 	products: productsReducer,
 });
 

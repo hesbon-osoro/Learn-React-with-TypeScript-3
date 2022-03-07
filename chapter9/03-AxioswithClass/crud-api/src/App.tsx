988d5173d45a0a9fb74f65b1936412add2eb37d3
wazimuhb <hesbonosoro1@gmail.com>
2022-03-07 18:09:29 +0300

    CRUD App

---
+++ chapter9/03-AxioswithClass/crud-api/src/App.tsx
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+interface IPost {
+	userId: number;
+	id?: number;
+	title: string;
+	body: string;
+}
+
+interface IState {
+	posts: IPost[];
+	error: string;
+}
+
+class App extends Component<{}, IState> {
+	public constructor(props: {}) {
+		super(props);
+		this.state = { posts: [], error: '' };
+	}
+	public componentDidMount() {
+		axios
+			.get<IPost[]>('https://jsonplaceholder.typicode.com/posts')
+			.then(response => {
+				this.setState({ posts: response.data });
+			})
+			.catch(err => {
+				const error =
+					err.response.status === 404
+						? 'Resource not found'
+						: 'An unexpected error has occurred';
+				this.setState({ error });
+			});
+	}
+	public render() {
+		return (
+			<div className="App">
+				<ul className="posts">
+					{this.state.posts.map(post => (
+						<li key={post.id}>
+							<h3>{post.title}</h3>
+							<p>{post.body}</p>
+						</li>
+					))}
+				</ul>
+				{this.state.error && <p className="error">{this.state.error}</p>}
+			</div>
+		);
+	}
+}
+export default App;

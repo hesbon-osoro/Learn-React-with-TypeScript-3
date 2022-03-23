6a97439bf40595bb9eb5850fa9e5a1ff4e1b19cb
wazimuhb <hesbonosoro1@gmail.com>
2022-03-23 16:15:23 +0300

    refactored component testing code | not showing code implementation

--- chapter11/01-ReactShop/src/ContactUs.test.tsx
+++ chapter11/01-ReactShop/src/ContactUs.test.tsx
@@ -3,7 +3,27 @@ import ReactDOM from 'react-dom';
 import { Simulate } from 'react-dom/test-utils';
 import ContactUs from './ContactUs';
 import { ISubmitResult } from './Form';
+import { render, cleanup } from '@testing-library/react';
 
+afterEach(cleanup);
+describe('ContactUs', () => {
+	test('When submit without filling in fields should display errors', () => {
+		const handleSubmit = async (): Promise<ISubmitResult> => {
+			return {
+				success: true,
+			};
+		};
+		const { container, getAllByText } = render(
+			<ContactUs onSubmit={handleSubmit} />
+		);
+		const form = container.querySelector('form');
+		expect(form).not.toBeNull();
+		Simulate.submit(form!);
+
+		const errorSpans = getAllByText('This must be populated');
+		expect(errorSpans.length).toBe(2);
+	});
+});
 // describe('ContactUs', () => {
 // 	test('When submit without filling in fields should display errors', () => {
 // 		const handleSubmit = async (): Promise<ISubmitResult> => {

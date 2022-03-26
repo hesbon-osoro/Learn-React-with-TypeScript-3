bc256c83d92404d9836eabfb1d20b55373cc849b
wazimuhb <hesbonosoro1@gmail.com>
2022-03-26 13:10:08 +0300

    some refactoring

--- chapter11/01-ReactShop/src/ContactUs.test.tsx
+++ chapter11/01-ReactShop/src/ContactUs.test.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Simulate } from 'react-dom/test-utils';
 import ContactUs from './ContactUs';
 import { ISubmitResult } from './Form';
-import { render, cleanup } from '@testing-library/react';
+import { render, cleanup, fireEvent } from '@testing-library/react';
 
 afterEach(cleanup);
 describe('ContactUs', () => {
@@ -13,15 +13,25 @@ describe('ContactUs', () => {
 				success: true,
 			};
 		};
-		const { container, getAllByText } = render(
+		const { getAllByText, getByText } = render(
 			<ContactUs onSubmit={handleSubmit} />
 		);
-		const form = container.querySelector('form');
-		expect(form).not.toBeNull();
-		Simulate.submit(form!);
+
+		const submitButton = getByText('Submit');
+		fireEvent.click(submitButton);
 
 		const errorSpans = getAllByText('This must be populated');
 		expect(errorSpans.length).toBe(2);
+	});
+	test('When submit with filling in fields should display okay', () => {
+		const handleSubmit = async (): Promise<ISubmitResult> => {
+			return {
+				success: true,
+			};
+		};
+		const { container, getByText, getByLabelText } = render(
+			<ContactUs onSubmit={handleSubmit} />
+		);
 	});
 });
 // describe('ContactUs', () => {
